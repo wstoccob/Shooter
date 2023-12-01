434e41bc3c4885a56e20e8181a52813a06d9572e
wstoccob <alinur.seysekov@gmail.com>
2023-12-01 13:50:13 +0600

    randomNumberGenerator
    -created RandomNumberGenerator (very wise methodology from authors, like mult. max value by double value within [0, 1), very good!)

--- wstoccob/Engine/Particles/EmitterTypes/EmitterParticleState.cs
+++ wstoccob/Engine/Particles/EmitterTypes/EmitterParticleState.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Dynamic;
+using Microsoft.Xna.Framework;
 
 namespace wstoccob.Engine.Particles.EmitterTypes
 {
     public abstract class EmitterParticleState
     {
-    
+        public abstract int MinLifeSpan { get; }
+        public abstract int MaxLifeSpan { get; }
+
+        public abstract float Velocity { get; }
+        public abstract float VelocityDeviation { get; }
+        public abstract float Acceleration { get; }
+        public abstract Vector2 Gravity { get; }
+        
+        public abstract float Opacity { get; }
+        public abstract float OpacityDeviation { get; }
+        public abstract float OpacityFadingRate { get; }
+
+        public abstract float Rotation { get; }
+        public abstract float RotationDeviation { get; }
+
+        public abstract float Scale { get; }
+        public abstract float ScaleDeviation { get; }
+        
+        
     }
 }
